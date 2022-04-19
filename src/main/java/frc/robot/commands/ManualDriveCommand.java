6a84ee2c02f7c7d2c9c2cd939869cbd0ed547e47
Gold87 <thethunder428@gmail.com>
2022-04-19 14:39:54 -0400

    Added a Manual Drive interrupt condition

--- src/main/java/frc/robot/commands/ManualDriveCommand.java
+++ src/main/java/frc/robot/commands/ManualDriveCommand.java
@@ -35,7 +35,11 @@ public class ManualDriveCommand extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    if(interrupted) {
+      driveSubsystem.manualDrive(0, 0);
+    }
+  }
 
   // Returns true when the command should end.
   @Override
