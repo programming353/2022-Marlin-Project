6fc8b57e1a0fb5a8fe7e14af0c356bdc8b00f2de
Gold87 <thethunder428@gmail.com>
2022-04-24 14:36:10 -0400

    Added NavX sensor

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,9 +4,14 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -18,24 +23,42 @@ public class DriveSubsystem extends SubsystemBase {
   private CANSparkMax frontRightMotor = new CANSparkMax(DriveConstants.frontRightMotorID, MotorType.kBrushless);
   private CANSparkMax backRightMotor = new CANSparkMax(DriveConstants.backRightMotorID, MotorType.kBrushless);
 
+  private RelativeEncoder leftEncoder = frontLeftMotor.getEncoder();
+  private RelativeEncoder rightEncoder = frontRightMotor.getEncoder();
+
   private MotorControllerGroup leftMotors = new MotorControllerGroup(frontLeftMotor, backLeftMotor);
   private MotorControllerGroup rightMotors = new MotorControllerGroup(frontRightMotor, backRightMotor);
 
   private DifferentialDrive differentialDrive;
+
+  private DifferentialDriveOdometry driveOdometry;
+  private double conversionToMeters = 0.004267;
+
+  private AHRS navXSensor = new AHRS(SPI.Port.kMXP);
 
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
     rightMotors.setInverted(true);
 
     differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+    differentialDrive.setSafetyEnabled(true);
+
+    driveOdometry = new DifferentialDriveOdometry(navXSensor.getRotation2d());
   }
 
   public void manualDrive(double forward, double turn) {
     differentialDrive.arcadeDrive(forward, turn);
+    differentialDrive.feed();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    driveOdometry.update(navXSensor.getRotation2d(), leftEncoder.getPosition() * conversionToMeters,
+        rightEncoder.getPosition() * conversionToMeters);
+  }
+
+  public Pose2d getPose() {
+    return driveOdometry.getPoseMeters();
   }
 }
