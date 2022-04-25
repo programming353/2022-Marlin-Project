799605d501e4c5c9bfa1721ccc5c062464d28d1d
Gold87 <thethunder428@gmail.com>
2022-04-24 21:27:45 -0400

    Changed joystick ramping rate

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -11,6 +11,9 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.BangBangController;
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -28,17 +31,14 @@ public class DriveSubsystem extends SubsystemBase {
   private MotorControllerGroup leftMotors = new MotorControllerGroup(frontLeftMotor, backLeftMotor);
   private MotorControllerGroup rightMotors = new MotorControllerGroup(frontRightMotor, backRightMotor);
 
-  private RelativeEncoder frontLeftEncoder = frontLeftMotor.getEncoder();
-  private RelativeEncoder backLeftEncoder = backLeftMotor.getEncoder();
-  private RelativeEncoder frontRightEncoder = frontRightMotor.getEncoder();
-  private RelativeEncoder backRightEncoder = backRightMotor.getEncoder();
-
   private DifferentialDrive differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
 
-  BangBangController bbController = new BangBangController();
+  private DifferentialDriveOdometry driveOdometry;
+  private double conversionToMeters = 0.004267;
 
-  SlewRateLimiter slewFilter = new SlewRateLimiter(1.0);
+  private AHRS navXSensor = new AHRS(SPI.Port.kMXP);
 
+  SlewRateLimiter slewFilter = new SlewRateLimiter(0.5);
 
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
@@ -51,20 +51,7 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   public void manualDrive(double forward, double turn) {
-    slewFilter.calculate(1.0);
-
     differentialDrive.arcadeDrive(slewFilter.calculate(forward), turn);
-
-    //double setpoint = 1.0; //there will be one for each motor
-    //double frontLeftDistance = setpoint - frontLeftEncoder.getPosition();
-    //frontLeftMotor.set(bbController.calculate(frontLeftDistance, setpoint));
-    //double backLeftDistance = setpoint - backLeftEncoder.getPosition();
-    //backLeftMotor.set(bbController.calculate(backLeftDistance, setpoint));
-    //double frontRightDistance = setpoint - frontRightEncoder.getPosition();
-    //frontRightMotor.set(bbController.calculate(frontRightDistance, setpoint));
-    //double backRightDistance = setpoint - backRightEncoder.getPosition();
-    //backRightMotor.set(bbController.calculate(backRightDistance, setpoint));
-    
   }
 
   @Override
