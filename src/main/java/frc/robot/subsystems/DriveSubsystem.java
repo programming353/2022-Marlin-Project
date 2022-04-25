1b4e4ef348f58457eaa9946ec4af44c1577ec198
Michael Iryami <michael.a.iryami@gmail.com>
2022-04-24 20:30:41 -0400

    Slew and Bang Bang implementations
    Bang Bang is commented

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,8 +5,11 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.BangBangController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -21,14 +24,37 @@ public class DriveSubsystem extends SubsystemBase {
   private MotorControllerGroup leftMotors = new MotorControllerGroup(frontLeftMotor, backLeftMotor);
   private MotorControllerGroup rightMotors = new MotorControllerGroup(frontRightMotor, backRightMotor);
 
+  private RelativeEncoder frontLeftEncoder = frontLeftMotor.getEncoder();
+  private RelativeEncoder backLeftEncoder = backLeftMotor.getEncoder();
+  private RelativeEncoder frontRightEncoder = frontRightMotor.getEncoder();
+  private RelativeEncoder backRightEncoder = backRightMotor.getEncoder();
+
   private DifferentialDrive differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+
+  BangBangController bbController = new BangBangController();
+
+  SlewRateLimiter slewFilter = new SlewRateLimiter(1.0);
+
 
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
   }
 
   public void manualDrive(double forward, double turn) {
-    differentialDrive.arcadeDrive(forward, turn);
+    slewFilter.calculate(1.0);
+
+    differentialDrive.arcadeDrive(slewFilter.calculate(forward), turn);
+
+    //double setpoint = 1.0; //there will be one for each motor
+    //double frontLeftDistance = setpoint - frontLeftEncoder.getPosition();
+    //frontLeftMotor.set(bbController.calculate(frontLeftDistance, setpoint));
+    //double backLeftDistance = setpoint - backLeftEncoder.getPosition();
+    //backLeftMotor.set(bbController.calculate(backLeftDistance, setpoint));
+    //double frontRightDistance = setpoint - frontRightEncoder.getPosition();
+    //frontRightMotor.set(bbController.calculate(frontRightDistance, setpoint));
+    //double backRightDistance = setpoint - backRightEncoder.getPosition();
+    //backRightMotor.set(bbController.calculate(backRightDistance, setpoint));
+    
   }
 
   @Override
